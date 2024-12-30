63b559c8b9449200fe4da20369015fdbe638628b
RaghavShiva <shiva26122004@gmail.com>
2024-12-31 00:07:05 +0530

    ui done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import Products from './components/Products'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import ProductDetails from './components/ProductDetails'
