13b0f815c38ed7545e42868d0409e1956320d06f
SHIVA RAGHAV <139120737+RaghavShiva@users.noreply.github.com>
2024-12-31 00:45:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import Products from './components/Products.jsx'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import ProductDetails from './components/ProductDetails.jsx'
 import SearchItems from './components/SearchItems.jsx'
-import Cart from './components/cart.jsx'
+import Cart from './components/Cart.jsx'
 import Account from './components/Account.jsx'
-import { items } from './components/data.jsx'
+import { items } from './components/Data.jsx'
 
 const App = () => {
   const [data, setData] = useState([...items])
